2a3cfadb4ed0fe8c519396331fb9788830115fad
Lukas Elsner <open@mindrunner.de>
2018-10-04 23:04:38 +0200

    add more sample code

--- app/main.js
+++ app/main.js
@@ -10,3 +10,10 @@ for (var i = 0; i <= 10; i++) {
 }
 
 blockchain.validateChain();
+
+let inducedErrorBlocks = [2,4,7];
+for (var i = 0; i < inducedErrorBlocks.length; i++) {
+    blockchain.chain[inducedErrorBlocks[i]].data='induced chain error';
+}
+
+blockchain.validateChain();
